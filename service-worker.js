978fed566ed67277f14fd11703b7299529c4f72f
amittgofficial <amittgofficial@gmail.com>
2025-12-07 14:06:20 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,45 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
+const cacheName = 'Code-cache-v2'; // Change version number when updating
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install event - caching files
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
-self.addEventListener("activate", e => {
-  console.log("Service Worker Activated");
+// Activate event - clean up old caches
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+// Fetch event - network first, then cache
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
+
+// Listen for skipWaiting message
+self.addEventListener('message', (event) => {
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
